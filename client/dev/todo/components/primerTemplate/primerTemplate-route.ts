b48fb3e37a8ac7738f6d9e92203d24674e5bac7c
boke <ojedabotansantiago@gmail.com>
2017-12-06 15:37:47 +0100

    modificado router primer template

--- client/dev/todo/components/primerTemplate/primerTemplate-route.ts
+++ client/dev/todo/components/primerTemplate/primerTemplate-route.ts
@@ -1,4 +1,4 @@
-import {
+/*import {
    Routes,
   RouterModule
 } from "@angular/router";
@@ -7,7 +7,7 @@ import {
    primerTemplateCmp
    } from "../../components/primerTemplate/primerTemplate-cmp";
 
-const todoRoutes: Routes = [
+const primerTemplateRoute: Routes = [
   {
     path: "",
     component: primerTemplateCmp,
@@ -15,4 +15,5 @@ const todoRoutes: Routes = [
   }
 ];
 
-export const todoRouting = RouterModule.forRoot(todoRoutes);
+export const pimerTemplateRouting = RouterModule.forRoot(primerTemplateRoute);
+*/
--- client/dev/todo/components/todos/todo-route.ts
+++ client/dev/todo/components/todos/todo-route.ts
@@ -7,12 +7,25 @@ import {
 	TodoCmp
 } from "../../components/todos/todo-cmp";
 
-const todoRoutes:Routes = [
-	{
-		path: "",
-		component: TodoCmp,
-		pathMatch: "full"
-	}
-]
+import {
+   primerTemplateCmp
+   } from "../../components/primerTemplate/primerTemplate-cmp";
+
+const todoRoutes: Routes = [
+  {
+    path: "TodoCmp",
+    component: TodoCmp,
+    pathMatch: "full"
+  }
+];
+
+const primerTemplateRoute: Routes = [
+  {
+    path: "/primerTemplateCmp",
+    component: primerTemplateCmp,
+    pathMatch: "full"
+  }
+];
 
 export const todoRouting = RouterModule.forRoot(todoRoutes);
+export const pimerTemplateRouting = RouterModule.forRoot(primerTemplateRoute);
